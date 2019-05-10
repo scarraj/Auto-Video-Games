f35005c0afc9c5f35246bb62197eb116b25ffc54
liy111 <liy111@miamioh.edu>
2019-05-10 15:26:17 -0400

    path updated for msbuild

--- test.bat
+++ test.bat
@@ -1,5 +1,5 @@
 @echo off
-"C:\Visiual Studio\MSBuild\15.0\Bin\MSBuild.exe" "C:\CSE201\CSE 201.sln" /t:build /p:Configuration=Release
+"C:\Program Files(x86)\Microsoft Visual Studio\2017\Enterprise\MSBuild\15.0\Bin\MSBuild.exe" "C:\CSE201\CSE 201.sln" /t:build /p:Configuration=Release
 
 cd \Program Files (x86)\IIS Express
 
